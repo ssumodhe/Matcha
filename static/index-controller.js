cbdf7c3109c83ceceb087d22dfa48564586f96e5
Safaiya SUMODHEE <ssumodhe@e2r7p23.42.fr>
2018-01-08 18:14:25 +0100

    starting JS control for form in index

---
+++ static/index-controller.js
@@ -0,0 +1,7 @@
+onfocus and onblur : pour quand on arrive ou quitte un input
+https://www.w3schools.com/jsref/event_onblur.asp
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onblur
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onblur_onfocus
+
+Check reg ex match with:
+https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_match_regexp
