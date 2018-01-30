2ceb18ded775cc128a4c4a71b703a9aa6a99e65f
Safaiya SUMODHEE <ssumodhe@e3r4p22.42.fr>
2018-01-30 18:43:08 +0100

    add two users in seed.py

--- seed.py
+++ seed.py
@@ -10,8 +10,31 @@ User.create({
 	'password': generate_password_hash('fy3wifEE'),
 	'confirmed': '1',
 	'age': '25',
-	'sex': '0'})
+	'sex': '1'})
 
+User.create({
+	'username': 'ketchup',
+	'first_name': 'Thomas',
+	'last_name': 'P',
+	'email': 'thomas@payet.ru',
+	'password': generate_password_hash('QWErty123'),
+	'confirmed': '1',
+	'age': '25',
+	'sex': '1',
+	'orientation': '1',
+	'bio': 'homme hetero'})
+
+User.create({
+	'username': 'hello',
+	'first_name': 'Elodie',
+	'last_name': 'D',
+	'email': 'elodie@d.fr',
+	'password': generate_password_hash('QWErty123'),
+	'confirmed': '1',
+	'age': '18',
+	'sex': '2',
+	'orientation': '1',
+	'bio': 'femme hetero'})
 Notification.create({
 	'user_id': '1',
 	'message': 'Hey just setting up my notifications'
