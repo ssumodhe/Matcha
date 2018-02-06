964a331864091ff76a8902d148ca6c6b2b72ec7b
Safaiya SUMODHEE <ssumodhe@e1r10p21.42.fr>
2018-02-06 18:25:12 +0100

    error and all_ok messages correctly displayed

--- controllers/root_controller.py
+++ controllers/root_controller.py
@@ -39,29 +39,29 @@ class RootController:
 				return redirect(url_for('home'))
 			else:
 				return redirect(url_for('profile', username=session['user']))
-		return render_template('index.html')
+		return render_template('index.html', error=None, all_ok=None)
 
 	@staticmethod
 	def signup(form):
 		for item in form.values():
 			if item == '' or item == None:
 				error = "REMPLIS TOUS LES P***** DE CHAMPS!"
-				return render_template('index.html', error=error)
+				return render_template('index.html', error=error, all_ok=None)
 
 		auth = User.find_by('username', form['username'])
 		if auth != None:
 			error = "Ce pseudo existe déjà!"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 
 		auth = User.find_by('email', form['email'])
 		if auth != None:
 			error = "Cet email existe déjà!"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 
 		# Prendre REGEX du JS in index-controller.js ???
 		if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", form['email']):
 			error = "P***** mais ecris bien ton email!"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 
 		if not re.match(r"(^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9_#@%\*\-]{8,24}$)", form['password']):
 			error = "Il s'agirait tout de meme de faire un effort sur la sécurité de ton mot de passe!"
@@ -69,7 +69,7 @@ class RootController:
 
 		if form['password'] != form['password_2']:
 			error = "P*****, mets les meme mots de passe!"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 
 		# All Good on procède à la création du User
 		infos = form.to_dict()
@@ -80,29 +80,29 @@ class RootController:
 		new_user = User.create(infos)
 
 		all_ok = "C'est Good!"
-		return render_template('index.html', all_ok=all_ok)
+		return render_template('index.html', all_ok=all_ok, error=None)
 
 	@staticmethod
 	def signin(form):
 		for item in form.values():
 			if item == '' or item == None:
 				error = "REMPLIS TOUS LES P***** DE CHAMPS!"
-				return render_template('index.html', error=error)
+				return render_template('index.html', error=error, all_ok=None)
 
 		auth = User.find_by('username', form['username'].lower())
 		if auth == None:
 			error = "On ne connait pas ce pseudo!"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 			
 		actual_pswd = auth.getPassword()
 		hashed_pswd = check_password_hash(actual_pswd, form['password'])
 		if hashed_pswd == False:
 			error = "Mot de passe incorrect!"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 
 		if auth.getConfirmed() == '0':
 			error = "Ton compte n'a pas été confirmé! Vérifie tes mails pour confirmer ton compte! :)"
-			return render_template('index.html', error=error)
+			return render_template('index.html', error=error, all_ok=None)
 
 		# All Good on modifie les infos nécessaires
 		auth.modif('status', '1')
